68380cde065f8fef48cd0d4b985d692e97d61b20
Shabad Sobti <shabad.sobti@gmail.com>
2019-02-04 14:39:40 -0500

    Migrations

--- paintings/models.py
+++ paintings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Paitings(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.CharField(max_length=100)
+    artist = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    medium = models.CharField(max_length=30)
+    price = models.IntegerField()
