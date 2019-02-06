a80e1daffd9c4e854f2bebec4352e9fab1bb11e0
rs4da <rs4da@virginia.edu>
2019-02-05 20:32:07 -0500

    fixed one-to-many order

--- orders/models.py
+++ orders/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Order(models.Model):
     timestamp = models.DateTimeField(blank=True)
     item = models.ForeignKey('paintings.Painting', on_delete=models.CASCADE)
+    buyer = models.ForeignKey(
+        'accounts.Buyer', on_delete=models.CASCADE)
+    seller = models.ForeignKey(
+        'accounts.Seller', on_delete=models.CASCADE)
