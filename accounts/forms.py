3ca40e6b78cf89a0ec061292d1923640f5e8cce7
Shabad Sobti <shabad.sobti@gmail.com>
2019-02-06 00:52:19 -0500

    Seller API

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.forms import ModelForm
-from .models import Buyer
+from .models import Buyer, Seller
 
 class BuyerForm(ModelForm):
     class Meta:
         model = Buyer
         fields = ['email', 'password', 'first_name', 'last_name', 'phone', 'shipping']
+
+class SellerForm(ModelForm):
+    class Meta:
+        model = Seller
+        fields = ['email', 'password', 'first_name', 'last_name', 'phone', 'description','logo']
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from .models import Buyer, Seller
 from django.http import HttpResponse
 from .models import Buyer, Seller
 from django.http import JsonResponse
-from .forms import BuyerForm
+from .forms import BuyerForm, SellerForm
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -19,6 +19,25 @@ def get_buyers_by_id(request, id):
     elif request.method == 'POST':
         buyer = Buyer.objects.get(pk=id)
         form = BuyerForm(request.POST, instance=buyer)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=400)
+
+
+def get_sellers(request):
+    data = list(Seller.objects.values())
+    return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def get_sellers_by_id(request, id):
+    if request.method == 'GET':
+        data = list(Seller.objects.values().filter(id = id))
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        seller = Seller.objects.get(pk=id)
+        form = SellerForm(request.POST, instance=seller)
         if form.is_valid():
             form.save()
             return HttpResponse(status=200)
