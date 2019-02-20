99f90af9facb985c7cced7f563e3eec568f7c6b3
Shabad Sobti <shabad.sobti@gmail.com>
2019-02-20 02:20:56 -0500

    Home Page banner done

--- web/home/views.py
+++ web/home/views.py
@@ -10,10 +10,16 @@ def get_home(request):
     r = requests.get('http://exp-api:8000/home/recent_paintings/')
     context = {
         "latest_paintings": get_latest_paintings(),
+        "banners": get_banners(),
     }
     return render(request, "home.html", context)
 
 
 def get_latest_paintings():
     r = requests.get('http://exp-api:8000/home/recent_paintings/')
+    return r.json
+
+
+def get_banners():
+    r = requests.get('http://exp-api:8000/home/banner/')
     return r.json
