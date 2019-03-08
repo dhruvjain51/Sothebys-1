fb9144ff34897b2f8abb64eba6a989ffd3bbac42
rs4da <rs4da@virginia.edu>
2019-03-08 22:12:50 +0300

    forgot to add authenticator to forms.py

--- models/accounts/forms.py
+++ models/accounts/forms.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.forms import ModelForm
-from .models import Buyer, Seller
+from .models import Buyer, Seller, Authenticator
 
 class BuyerForm(ModelForm):
     class Meta:
@@ -11,3 +11,8 @@ class SellerForm(ModelForm):
     class Meta:
         model = Seller
         fields = ['email', 'password', 'first_name', 'last_name', 'phone', 'description','logo']
+
+class AuthenticatorForm(ModelForm):
+    class Meta:
+        model = Authenticator
+        fields = ['authenticator', 'user_id', 'date_created']
