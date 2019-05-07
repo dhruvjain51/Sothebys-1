c467829c25dec1e941b1235d68419a10391add75
rs4da <rs4da@virginia.edu>
2019-05-07 14:06:38 -0400

    got the first spark prog to run

--- data/hello.py
+++ data/hello.py
@@ -1,11 +1,10 @@
 from pyspark import SparkContext
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
-data = sc.textFile("/tmp/data/access_log.txt", 2)     # each worker loads a piece of the data file
 
-# conn = MySQLdb.connect(host='db', port=3306, user='www', passwd='$3cureUS', db='cs4501')
+data = sc.textFile("/tmp/data/access.log", 2)     # each worker loads a piece of the data file
 
-pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
+pairs = data.map(lambda line: line.split(" "))   # tell each worker to split each line of it's partition
 pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
 count = pages.reduceByKey(lambda x,y: int(x)+int(y))        # shuffle the data so that each key is only on one worker
                                                   # and then reduce all the values by adding them together
